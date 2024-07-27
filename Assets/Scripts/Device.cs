88550ffe9f17b94d5b4a75752e8548db58be8021
Mal <malachaiwg@gmail.com>
2024-07-28 01:10:45 +1200

    Remove namespace from Device

--- Assets/Scripts/Device.cs
+++ Assets/Scripts/Device.cs
@@ -1,24 +1,21 @@
 using UnityEngine;
 
-namespace DefaultNamespace
+public abstract class Device : MonoBehaviour
 {
-    public abstract class Device : MonoBehaviour
+    public Vector3Int position;
+    public virtual Vector3Int[] Inputs => new[] {GetLocalDirection(Vector3.back)};
+
+    protected virtual void Awake()
     {
-        public Vector3Int position;
-        public virtual Vector3Int[] Inputs => new[] {GetLocalDirection(Vector3.back)};
-
-        protected virtual void Awake()
-        {
-            Vector3 worldPos = transform.position;
-            position = FromV3(worldPos);
-        }
-
-        protected Vector3Int GetLocalDirection(Vector3 direction)
-        {
-            Vector3 localDirection = transform.rotation * direction;
-            return FromV3(localDirection);
-        }
-
-        private Vector3Int FromV3(Vector3 pos) => new(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y), Mathf.RoundToInt(pos.z));
+        Vector3 worldPos = transform.position;
+        position = FromV3(worldPos);
     }
+
+    protected Vector3Int GetLocalDirection(Vector3 direction)
+    {
+        Vector3 localDirection = transform.rotation * direction;
+        return FromV3(localDirection);
+    }
+
+    public static Vector3Int FromV3(Vector3 pos) => new(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y), Mathf.RoundToInt(pos.z));
 }
