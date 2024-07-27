f1a4a865421892c6a3b8836753986ae4fa9af5ac
Mal <malachaiwg@gmail.com>
2024-07-27 17:03:49 +1200

    Remove rendundant function ItemTaker.SetupGive()

--- Assets/Scripts/ItemTaker.cs
+++ Assets/Scripts/ItemTaker.cs
@@ -8,11 +8,6 @@ public abstract class ItemTaker : Device
     public abstract void Take(Transform item, float startingDistance);
     public abstract bool CanTake(Transform item);
     
-    public void SetupGive(ItemTaker taker)
-    {
-        receiver = taker;
-    }
-    
     protected bool CanGive(Transform item) => receiver && receiver.CanTake(item);
 
     protected void Give(Transform item, float startingDistance)
