188c213303457001c64bee0896108819f7132ed8
Mal <malachaiwg@gmail.com>
2024-07-28 01:08:19 +1200

    TimeSpeedMultiplier improvement

--- Assets/Scripts/TimeSpeedMultiplier.cs
+++ Assets/Scripts/TimeSpeedMultiplier.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class TimeSpeedMultiplier : MonoBehaviour
 {
     [SerializeField, Range(0, 10)] private float speed = 1;
-
+    [SerializeField, Range(5, 144)] private float targetFramerate = 60;
     void Update()
     {
         Time.timeScale = speed;
+        Time.fixedDeltaTime = 1f / (targetFramerate / speed);
+        // Application.targetFrameRate = (int)(targetFramerate / speed);
     }
 }
