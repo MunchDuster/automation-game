5b7087a96bd07b9f035c25f42c1a79fd8667b938
Mal <malachaiwg@gmail.com>
2024-07-28 20:11:43 +1200

    move PathGeneration

--- Assets/Scripts/PathGenerator.cs
+++ Assets/Scripts/PathGenerator.cs
@@ -13,16 +13,21 @@ public class PathGenerator : MonoBehaviour
         GeneratePath();
     }
 
-    private void GeneratePath()
+    private int maxTries = 10;
+    
+    private void GeneratePath(int tryNo = 1)
     {
         ChunkManager manager = ChunkManager.Instance;
         
-        Vector3 curPosition = transform.position;
+        Vector3 curPosition = GetNearestUnoccupiedPosition(transform.position);
         Quaternion curRotation = Quaternion.Euler(0, 90 * Random.Range(0, 4), 0);
-        ItemTaker lastItem = Instantiate(extractorPrefab, curPosition, curRotation, transform);
+        Extractor extractor = Instantiate(extractorPrefab, curPosition, curRotation, transform);
+        ItemTaker lastItem = extractor;
         ItemTaker lastlastItem = lastItem;
         Quaternion lastRotation = curRotation;
         manager.AddDevice(lastItem);
+        
+        int conveyorsPlaced = 0;
         for (int i = 0; i < length; i++)
         {
             ConveyorBelt conveyorPrefab = conveyorPrefabs[Random.Range(0, conveyorPrefabs.Length)];
@@ -39,18 +44,45 @@ public class PathGenerator : MonoBehaviour
                 lastlastItem = lastItem;
                 lastItem = conveyorInstance;
                 manager.AddDevice(lastItem);
+                conveyorsPlaced++;
             }
         }
         
         // replace last item with extractor as we are sure that place was not colliding with something
         manager.RemoveDevice(lastItem);
         DestroyImmediate(lastItem.gameObject);
-        
+
+        if (conveyorsPlaced <= 1) // if one conveyor then extractor and storage will be directly face to face 
+        {
+            Debug.LogWarning("Did not place any conveyors, this path has failed.");
+            if (conveyorsPlaced == 1)
+            {
+                manager.RemoveDevice(extractor);
+                DestroyImmediate(extractor.gameObject);
+
+                if (tryNo >= maxTries)
+                {
+                    return;
+                }
+                GeneratePath(tryNo++);
+            }
+            return;
+        }
         lastRotation *= Quaternion.Euler(0, 180, 0);
         Storage storage = Instantiate(storagePrefab, curPosition, lastRotation, transform);
         lastlastItem.receiver = storage;
         manager.AddDevice(storage);
     }
 
-    private Vector3Int RandomDirection() => new(Random.Range(-1, 2), Random.Range(-1, 2), Random.Range(-1, 2));
+    private Vector3 GetNearestUnoccupiedPosition(Vector3 targetPos)
+    {
+        while (ChunkManager.Instance.CheckOccupied(Device.FromV3(targetPos)))
+        {
+            targetPos += RandomDirection();
+        }
+
+        return targetPos;
+    }
+
+    private Vector3Int RandomDirection() => new(Random.Range(-1, 2), 0, Random.Range(-1, 2));
 }
